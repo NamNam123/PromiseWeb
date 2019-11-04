310afec5cb03d46463e1469af2fd548a43a51c21
Nam Van Pham <nvph@netcompany.com>
2019-11-04 20:54:04 +0100

    Add project files.

---
+++ PromiseWeb/hello.js
@@ -0,0 +1,18 @@
+﻿function ShowHelloMessage() {
+    var name = document.getElementById("myname");
+    document.getElementById("hellomessage").innerHTML = "Hello, " + name.value;
+}
+promise2 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve({
+            message: "The man likes to keep his word",
+            code: "aManKeepsHisWord"
+        });
+
+       
+        var today = new Date();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        console.log(time)
+    }, 5000);
+});
+console.log(promise2);
